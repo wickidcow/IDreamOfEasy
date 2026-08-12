2419955848ab762ee46997b4ff748718450dbf66
wickidcow <jaywago@gmail.com>
2026-08-12 14:09:53 -0400

    Move Magnetoid item work to entity schedulers

--- src/main/java/me/bunnky/idreamofeasy/slimefun/tasks/MagnetoidTask.java
+++ src/main/java/me/bunnky/idreamofeasy/slimefun/tasks/MagnetoidTask.java
@@ -1,11 +1,12 @@
 package me.bunnky.idreamofeasy.slimefun.tasks;
 
 import io.github.thebusybiscuit.slimefun4.utils.SlimefunUtils;
+import io.papermc.paper.threadedregions.scheduler.ScheduledTask;
 import me.bunnky.idreamofeasy.IDreamOfEasy;
 import me.bunnky.idreamofeasy.listeners.MagnetoidListener;
 import me.bunnky.idreamofeasy.slimefun.items.Magnetoid;
-import org.bukkit.Bukkit;
 import org.bukkit.GameMode;
+import org.bukkit.Location;
 import org.bukkit.NamespacedKey;
 import org.bukkit.Sound;
 import org.bukkit.SoundCategory;
@@ -14,63 +15,92 @@ import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.persistence.PersistentDataType;
-import org.bukkit.scheduler.BukkitRunnable;
 import org.jetbrains.annotations.NotNull;
 
-public class MagnetoidTask extends BukkitRunnable {
+public final class MagnetoidTask {
 
     private static final float COST = 0.01F;
 
     private final double r;
-    private final Player p;
+    private final Player player;
     private final Magnetoid magnetoid;
     private final NamespacedKey teleportMarker;
 
-    public MagnetoidTask(@NotNull Player p, double r, @NotNull Magnetoid magnetoid) {
+    public MagnetoidTask(@NotNull Player player, double r, @NotNull Magnetoid magnetoid) {
         this.r = r;
-        this.p = p;
+        this.player = player;
         this.magnetoid = magnetoid;
         this.teleportMarker = new NamespacedKey(IDreamOfEasy.getInstance(), "magnetoid_tp");
     }
 
-    @Override
-    public void run() {
+    public void run(@NotNull ScheduledTask task) {
         if (!isValid()) {
-            MagnetoidListener.activeTasks.remove(p.getUniqueId());
-            cancel();
+            stop(task);
             return;
         }
 
-        ItemStack off = p.getInventory().getItemInOffHand();
-        if (!SlimefunUtils.isItemSimilar(off, magnetoid.getItem(), true)) {
-            MagnetoidListener.activeTasks.remove(p.getUniqueId());
-            cancel();
+        ItemStack offhand = player.getInventory().getItemInOffHand();
+        if (!SlimefunUtils.isItemSimilar(offhand, magnetoid.getItem(), true)) {
+            stop(task);
             return;
         }
 
-        if (!p.isSneaking()) {
-            for (Entity ent : p.getNearbyEntities(r, r, r)) {
-                if (ent instanceof Item item && isValidItem(item) && !isTeleportMarked(item)) {
-                    if (magnetoid.removeItemCharge(off, COST)) {
-                        item.teleport(p.getLocation());
-                        p.playSound(p, Sound.BLOCK_AMETHYST_BLOCK_STEP, SoundCategory.PLAYERS, 0.2F, 0.2F);
-                        markTeleported(item);
-                        Bukkit.getScheduler().runTaskLater(IDreamOfEasy.getInstance(), () -> clearTeleportMarker(item), 10L);
-                    }
-                }
+        if (player.isSneaking()) {
+            return;
+        }
+
+        for (Entity entity : player.getNearbyEntities(r, r, r)) {
+            if (!(entity instanceof Item item) || !isValidItem(item) || isTeleportMarked(item)) {
+                continue;
             }
+
+            if (!magnetoid.removeItemCharge(offhand, COST)) {
+                return;
+            }
+
+            Location destination = player.getLocation().clone();
+            player.playSound(player, Sound.BLOCK_AMETHYST_BLOCK_STEP, SoundCategory.PLAYERS, 0.2F, 0.2F);
+
+            item.getScheduler().execute(
+                IDreamOfEasy.getInstance(),
+                () -> teleportItem(item, destination),
+                null,
+                1L
+            );
         }
     }
 
-    protected boolean isValid() {
-        return p.isOnline() && p.getGameMode() != GameMode.SPECTATOR;
+    private void teleportItem(Item item, Location destination) {
+        if (!isValidItem(item) || isTeleportMarked(item)) {
+            return;
+        }
+
+        item.teleport(destination);
+        markTeleported(item);
+        item.getScheduler().runDelayed(
+            IDreamOfEasy.getInstance(),
+            ignored -> clearTeleportMarker(item),
+            null,
+            10L
+        );
+    }
+
+    private void stop(ScheduledTask task) {
+        MagnetoidListener.activeTasks.remove(player.getUniqueId(), task);
+        task.cancel();
+    }
+
+    private boolean isValid() {
+        return player.isOnline() && player.getGameMode() != GameMode.SPECTATOR;
     }
 
     private boolean isValidItem(Item item) {
-        return !item.isDead()
+        return item.isValid()
+            && !item.isDead()
             && !SlimefunUtils.hasNoPickupFlag(item)
             && item.getPickupDelay() <= 0
-            && p.getLocation().distanceSquared(item.getLocation()) > 0.3;
+            && player.getWorld() == item.getWorld()
+            && player.getLocation().distanceSquared(item.getLocation()) > 0.3;
     }
 
     private boolean isTeleportMarked(Item item) {
