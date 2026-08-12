9535541c1fd31f3bce2982d0f31e2faedee7a8c6
wickidcow <jaywago@gmail.com>
2026-08-12 13:37:48 -0400

    Modernize BiomeCompass for Paper 26.2 registries

--- src/main/java/me/bunnky/idreamofeasy/slimefun/items/BiomeCompass.java
+++ src/main/java/me/bunnky/idreamofeasy/slimefun/items/BiomeCompass.java
@@ -9,11 +9,15 @@ import io.github.thebusybiscuit.slimefun4.api.recipes.RecipeType;
 import io.github.thebusybiscuit.slimefun4.core.handlers.ItemUseHandler;
 import io.github.thebusybiscuit.slimefun4.implementation.Slimefun;
 import io.github.thebusybiscuit.slimefun4.implementation.items.SimpleSlimefunItem;
+import io.papermc.paper.registry.RegistryAccess;
+import io.papermc.paper.registry.RegistryKey;
 import me.bunnky.idreamofeasy.utils.IDOEUtility;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
 import org.bukkit.Particle;
+import org.bukkit.Registry;
 import org.bukkit.Sound;
 import org.bukkit.SoundCategory;
 import org.bukkit.block.Biome;
@@ -21,12 +25,13 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.util.BiomeSearchResult;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
 /*
 A useful tool that points players toward the nearest biome of their choice, aiding exploration.
@@ -36,19 +41,19 @@ public class BiomeCompass extends SimpleSlimefunItem<ItemUseHandler> {
 
     private final ItemSetting<Integer> r = new IntRangeSetting(this, "range", 1, 100, Integer.MAX_VALUE);
 
-    private static final int COOLDOWN_TICKS = 200; // 10 Seconds
-    private final Biome[] biomes;
+    private static final int COOLDOWN_TICKS = 200; // 10 seconds
 
-    private Map<Player, Integer> playerBiomeSelection = new HashMap<>();
-    private Map<Player, Map<Biome, List<Location>>> playerBiomeCache = new HashMap<>();
-    private Map<Player, Map<Biome, Location>> playerDiscoveredBiomes = new HashMap<>();
+    private final Registry<Biome> biomeRegistry;
+    private final List<Biome> biomes;
+    private final Map<UUID, Integer> playerBiomeSelection = new HashMap<>();
 
     public BiomeCompass(ItemGroup itemGroup, SlimefunItemStack item, RecipeType recipeType, ItemStack[] recipe) {
         super(itemGroup, item, recipeType, recipe);
         addItemSetting(r);
         IDOEUtility.setGlow(item);
 
-        this.biomes = Biome.values();
+        this.biomeRegistry = RegistryAccess.registryAccess().getRegistry(RegistryKey.BIOME);
+        this.biomes = biomeRegistry.stream().toList();
     }
 
     @Override
@@ -59,8 +64,9 @@ public class BiomeCompass extends SimpleSlimefunItem<ItemUseHandler> {
     @EventHandler(priority = EventPriority.LOWEST)
     private void onRightClick(@NotNull PlayerRightClickEvent e) {
         Player p = e.getPlayer();
+        UUID playerId = p.getUniqueId();
 
-        int selectedBiomeIndex = playerBiomeSelection.getOrDefault(p, 0);
+        int selectedBiomeIndex = playerBiomeSelection.getOrDefault(playerId, 0);
 
         if (p.hasCooldown(Material.COMPASS)) {
             p.sendMessage("§cYou must wait before using this again!");
@@ -69,27 +75,28 @@ public class BiomeCompass extends SimpleSlimefunItem<ItemUseHandler> {
 
         if (p.isSneaking()) {
             if (e.getInteractEvent().getClickedBlock() == null) {
-                selectedBiomeIndex = (selectedBiomeIndex + 1) % biomes.length;
-                playerBiomeSelection.put(p, selectedBiomeIndex);
+                selectedBiomeIndex = (selectedBiomeIndex + 1) % biomes.size();
             } else {
-                selectedBiomeIndex = (selectedBiomeIndex - 1 + biomes.length) % biomes.length;
-                playerBiomeSelection.put(p, selectedBiomeIndex);
+                selectedBiomeIndex = (selectedBiomeIndex - 1 + biomes.size()) % biomes.size();
             }
-            p.sendMessage("§eSelected biome: " + ChatColor.GOLD + biomes[selectedBiomeIndex].name());
+
+            playerBiomeSelection.put(playerId, selectedBiomeIndex);
+            p.sendMessage("§eSelected biome: " + ChatColor.GOLD + getBiomeName(biomes.get(selectedBiomeIndex)));
             return;
         }
 
-        Location closestBiomeLocation = findClosestBiome(p, p.getLocation(), biomes[selectedBiomeIndex]);
+        Biome selectedBiome = biomes.get(selectedBiomeIndex);
+        Location closestBiomeLocation = findClosestBiome(p.getLocation(), selectedBiome);
+
         if (closestBiomeLocation != null) {
             p.setCompassTarget(closestBiomeLocation);
             double distance = p.getLocation().distance(closestBiomeLocation);
-            p.sendMessage("§aFound " + ChatColor.GREEN + biomes[selectedBiomeIndex].name() + " §abiome, " + ChatColor.GREEN + (int) distance + " §ablocks away!");
-            Map<Biome, Location> playerDiscovered = playerDiscoveredBiomes.computeIfAbsent(p, k -> new HashMap<>());
-            playerDiscovered.put(biomes[selectedBiomeIndex], closestBiomeLocation);
+            p.sendMessage("§aFound " + ChatColor.GREEN + getBiomeName(selectedBiome) + " §abiome, "
+                + ChatColor.GREEN + (int) distance + " §ablocks away!");
             p.setCooldown(Material.COMPASS, COOLDOWN_TICKS);
             p.playSound(p, Sound.ENTITY_PLAYER_LEVELUP, SoundCategory.PLAYERS, 0.5F, 2F);
         } else {
-            p.sendMessage("§cNo " + ChatColor.DARK_RED + biomes[selectedBiomeIndex].name() + " §cbiome nearby.");
+            p.sendMessage("§cNo " + ChatColor.DARK_RED + getBiomeName(selectedBiome) + " §cbiome nearby.");
             p.setCooldown(Material.COMPASS, COOLDOWN_TICKS);
             p.playSound(p, Sound.BLOCK_ANVIL_LAND, SoundCategory.PLAYERS, 0.4F, 1F);
         }
@@ -97,48 +104,25 @@ public class BiomeCompass extends SimpleSlimefunItem<ItemUseHandler> {
         p.spawnParticle(Particle.EFFECT, p.getLocation().add(0.5, 0.5, 0.5), 30, 0.3, 0.3, 0.3, 0.05);
     }
 
-    private Location findClosestBiome(Player p, Location playerLocation, Biome targetBiome) {
-        List<Location> foundLocations = new ArrayList<>();
-        int radius = r.getValue();
+    private Location findClosestBiome(Location playerLocation, Biome targetBiome) {
+        BiomeSearchResult result = playerLocation.getWorld().locateNearestBiome(playerLocation, r.getValue(), targetBiome);
+        return result == null ? null : result.getLocation();
+    }
 
-        for (int x = -radius; x <= radius; x++) {
-            for (int z = -radius; z <= radius; z++) {
-                int biomeX = playerLocation.getBlockX() + x;
-                int biomeZ = playerLocation.getBlockZ() + z;
-                Biome biome = playerLocation.getWorld().getBiome(biomeX, biomeZ);
-                if (biome == targetBiome) {
-                    Location foundLocation = new Location(playerLocation.getWorld(), biomeX, playerLocation.getWorld().getHighestBlockYAt(biomeX, biomeZ), biomeZ);
-                    foundLocations.add(foundLocation);
-                }
+    private String getBiomeName(Biome biome) {
+        NamespacedKey key = biomeRegistry.getKey(biome);
+        if (key == null) {
+            return "Unknown";
+        }
+
+        StringBuilder name = new StringBuilder();
+        for (String part : key.getKey().split("_")) {
+            if (!name.isEmpty()) {
+                name.append(' ');
             }
+            name.append(Character.toUpperCase(part.charAt(0))).append(part.substring(1));
         }
-
-        if (!foundLocations.isEmpty()) {
-            Location closestNewBiome = foundLocations.get(0);
-            Map<Biome, List<Location>> playerCache = playerBiomeCache.computeIfAbsent(p, k -> new HashMap<>());
-            playerCache.computeIfAbsent(targetBiome, k -> new ArrayList<>()).add(closestNewBiome);
-
-            return closestNewBiome;
-        }
-
-        Map<Biome, List<Location>> playerCache = playerBiomeCache.computeIfAbsent(p, k -> new HashMap<>());
-        List<Location> cachedLocations = playerCache.get(targetBiome);
-
-        if (cachedLocations != null) {
-            Location closestCached = null;
-            double closestCachedDistance = Double.MAX_VALUE;
-
-            for (Location loc : cachedLocations) {
-                double distance = loc.distance(playerLocation);
-                if (distance < closestCachedDistance) {
-                    closestCachedDistance = distance;
-                    closestCached = loc;
-                }
-            }
-
-            return closestCached;
-        }
-        return null;
+        return name.toString();
     }
 
     public static int getRange() {
